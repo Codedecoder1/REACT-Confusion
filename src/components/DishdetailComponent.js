d9e5ad94894d84844173ebeb861f748ae2468643
Eudis Matos <eudismatos@yahoo.es>
2019-03-30 12:30:53 -0700

    Assignment-Week 1... Task1, Task 2 and Task3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React ,{ Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle } from 'reactstrap';
 import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
 
 class DishDetail extends Component {
 
@@ -25,17 +26,48 @@ class DishDetail extends Component {
             </div>
         );
 }
+
+renderComments(comments) {
+  if (comments != null)
+  return ( 
+    <div className="col-12 col-md-5 m-1">
+    <div>
+      <h1>Comments</h1>
+      <ul class="list-unstyled"> 
+        {comments.map((comment) => (
+            <li key={comment.id}> 
+            <div class="mb-3">{comment.comment} </div>
+            <div class="mb-3"> {comment.author} {comment.date}</div>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+
+    </div>
+  )
+  else
+    return (
+    <div>
+
+    </div>
+  );
+}
   render() {
     if (this.props.selectedDish != null)
     return(
   <div className="row">
-      {this.renderDish(this.props.selectedDish)}
+      {this.renderDish(this.props.selectedDish)} 
+     
+     {this.renderComments(this.props.selectedDish.comments)}
   </div>
+
+
     );
-    else 
+   else 
     return(
       <div class='row'>
- 
+
       </div>
     );
   }
